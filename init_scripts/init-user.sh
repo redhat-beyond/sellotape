95e3ae8259f375f98d7e2649beb3725fa685d1fd
Gal Lapid <11galpid@gmail.com>
2020-08-27 18:14:58 +0300

    Updated script files and README -Added comments that explain the usage and flow better -Readme now includes a basic step-by-step procedure for setting up the development machine

--- init_scripts/init-user.sh
+++ init_scripts/init-user.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# Install pipenv and then use it to install a snapshot of
-# the packages needed for the project
+# init-user.sh - Initialize user-level requirements for our project
+# Should be set in the Vagrantfilee to run once with low privileges (non-root).
+
+# Using pipenv as our default python package manager
+# Also, we specify the version to avoid possible inconsistency
+# with future releases, and --user to reduce global fingerprint
+# on the system as a whole.
 python -m pip install -I pipenv==2020.8.13 --user
 cd /vagrant/sellotape && python -m pipenv sync
 echo "-----2ND INIT DONE-----"
