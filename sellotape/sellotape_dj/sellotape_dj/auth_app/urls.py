bf57b67ff523a2184ec59ea32c313cd65532d996
Gal Lapid <11galpid@gmail.com>
2020-08-30 10:12:53 +0300

    Built django project & First app POC -Register & Login with Google & Facebook using social-auth-app-django -Currently includes mockup login & register page, just for basic functionality -Updated pipenv.lock to include social-auth-app-django

---
+++ sellotape/sellotape_dj/sellotape_dj/auth_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from sellotape_dj.auth_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),         path("login/", views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    path("", views.home, name="home"),
+]
