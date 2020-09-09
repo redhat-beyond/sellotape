a76dbf67ced4a1f389edd7631cc3bc0bce1ed193
Guy Itzhaki <itzhaki1234@gmail.com>
2020-09-09 16:59:52 +0300

    user_user_followers_model

--- sellotape/sellotape_dj/main_app/admin.py
+++ sellotape/sellotape_dj/main_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model
+class User(models.Model):
+	# Inherits Attributes from User default instance
+    user 	= models.OneToOneField(User, on_delete=models.CASCADE)
+    # Generates a user ID which is the primary identifier to the user
+    user_id	= models.AutoField(primary_key=True)
+    # User avatar, optional: limit the image instance height and width
+    avatar	= models.ImageField()
+    country	= models.CharField(default='Israel')
+    
+	class city(models.TextChoices):
+	     	tel_aviv 	= 'ta', _('Tel Aviv')
+	     	hamerkaz 	= 'me', _('HaMerkaz')
+	     	hadarom 	= 'da', _('HaDarom')
+	     	hatzafon 	= 'tz', _('HaTzafon')
+    city	= models.CharField(
+        max_length=2,
+        choices=city.choices,
+        default=city.hamerkaz,
+    ) 		 
+
+    
--- sellotape/sellotape_dj/main_app/models.py
+++ sellotape/sellotape_dj/main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class user_followers(models.Model):
+    user_followers_id 	=     user_id = models.AutoField(primary_key=True)
+    user_id 			= models.ForeignKey('self', on_delete=models.CASCADE)
+    follower_id 		= models.ForeignKey(User, on_delete=models.CASCADE)
