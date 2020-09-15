8266aadb8d87728efda23ff3f97e9c8243f6940c
Tarel <tarel@live.com>
2020-09-15 19:38:22 +0300

    little fixes - new lines

--- sellotape/sellotape_dj/chat/urls.py
+++ sellotape/sellotape_dj/chat/urls.py
@@ -1,5 +1,4 @@
-
-from django.urls import path, re_path
+from django.urls import path
 from .views import index, room
 
 
@@ -7,5 +6,5 @@ app_name = 'chat'
 
 urlpatterns = [
     path('', index, name = 'index'),
-    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
+    path('<str:room_name>/', room, name='room'),
 ]
--- sellotape/sellotape_dj/chat/views.py
+++ sellotape/sellotape_dj/chat/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.utils.safestring import mark_safe
-import json
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -10,3 +8,4 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+    
