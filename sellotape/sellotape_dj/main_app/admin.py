fcdf29c5703e2a3601050cb37243eea02c4d1443
Guy Itzhaki <itzhaki1234@gmail.com>
2020-09-16 15:24:46 +0300

    Changes to ORM, UserProfile and UserFollowers PR

--- sellotape/sellotape_dj/main_app/admin.py
+++ sellotape/sellotape_dj/main_app/admin.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
+from django.db import models
 from django.contrib.auth.models import User
 
 # Register your models here.
 
 # https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model
-class User(models.Model):
-	# Inherits Attributes from User default instance
-    user 	= models.OneToOneField(User, on_delete=models.CASCADE)
-    # Generates a user ID which is the primary identifier to the user
-    user_id	= models.AutoField(primary_key=True)
+class UserProfile(models.Model):
+    # Inherits Attributes from User default instance
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     # User avatar, optional: limit the image instance height and width
-    avatar	= models.ImageField()
-    country	= models.CharField(default='Israel')
+    avatar = models.ImageField()
+    country = models.CharField(max_length=100, default='Israel')
     
-	class city(models.TextChoices):
-	     	tel_aviv 	= 'ta', _('Tel Aviv')
-	     	hamerkaz 	= 'me', _('HaMerkaz')
-	     	hadarom 	= 'da', _('HaDarom')
-	     	hatzafon 	= 'tz', _('HaTzafon')
-    city	= models.CharField(
-        max_length=2,
-        choices=city.choices,
-        default=city.hamerkaz,
-    ) 		 
+    class City(models.IntegerChoices):
+        tel_aviv = '1', 'Tel Aviv'
+        hamerkaz = '2', 'HaMerkaz'
+        hadarom = '3', 'HaDarom'
+        hatzafon = '4', 'HaTzafon'
 
-    
+    city = models.IntegerField(
+        choices=City.choices,
+        default=City.tel_aviv,
+    )   
--- sellotape/sellotape_dj/main_app/models.py
+++ sellotape/sellotape_dj/main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserFollowers(models.Model):
+    user_followers_id = user_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    follower_id	= models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Stream(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     link = models.URLField(max_length=500, blank=False)
