8d3e08feeac49e0cf3f159b9f72103b7a0097f54
Gal Lapid <11galpid@gmail.com>
2020-09-19 18:23:57 +0300

    Created initial version of the Sellotape template

--- sellotape/sellotape_dj/main_app/views.py
+++ sellotape/sellotape_dj/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'sellotape/index.html')
+
+def landing_page(request):
+    return render(request, 'landing.html')
