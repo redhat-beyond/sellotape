b4bad8835ddeb51da4fd150125861ed9879c09a4
Omri Rosner <68001413+rosomri@users.noreply.github.com>
2020-09-21 15:31:57 +0300

    uptade models

--- sellotape/sellotape_dj/main_app/models.py
+++ sellotape/sellotape_dj/main_app/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Stream(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     link = models.URLField(max_length=500, blank=False)
+    title = models.CharField(max_length=50, null=True)
+    description = models.TextField(max_length=50, null=True)
     airs_on = models.DateField()
     ends_on = models.DateField(null=True)
     added_on = models.DateField()
