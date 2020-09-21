58fcfd6308b8c30fbcab148010ca89a8520e8f3b
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-09-21 13:18:09 +0300

    Added the relevant html and tempalte conditions to show an appropriate message when a user exist but has no streams. Note that the main_app namespace was added to the main urls.py file of the project.

--- sellotape/sellotape_dj/sellotape_dj/urls.py
+++ sellotape/sellotape_dj/sellotape_dj/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('', include(('main_app.urls', 'main_app'), namespace='main_app'))
 ]
