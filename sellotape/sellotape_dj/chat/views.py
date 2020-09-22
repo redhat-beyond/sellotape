4995cee812d18e63f11efca58f9642ee44283588
Tarel <tarel@live.com>
2020-09-22 10:09:07 +0300

    Upgraded Chat-App

--- sellotape/sellotape_dj/chat/views.py
+++ sellotape/sellotape_dj/chat/views.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
 from django.shortcuts import render
+import json
 
 def index(request):
     return render(request, 'chat/index.html')
-
+@login_required
 def room(request, room_name):
-    print(room_name)
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username': mark_safe(json.dumps(request.user.username)),
     })
+    
