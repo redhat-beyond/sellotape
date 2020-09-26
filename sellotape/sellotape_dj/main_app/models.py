cc129cf3135782d026750c642161b9ed86e7dab4
Omri Rosner <68001413+rosomri@users.noreply.github.com>
2020-09-26 20:19:14 +0300

    Please enter the commit message for your changes. Lines starting

--- sellotape/sellotape_dj/main_app/models.py
+++ sellotape/sellotape_dj/main_app/models.py
@@ -1,39 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model
+
 class Profile(models.Model):
     # Inherits Attributes from User default instance
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # User avatar, optional: limit the image instance height and width
     avatar = models.ImageField()
     country = models.CharField(max_length=100, default='Israel')
-    youtube_link = models.URLField(max_length=200)
-    twitch_link = models.URLField(max_length=200)
-    
+    youtube_link = models.URLField(max_length=200, blank=True)
+    twitch_link = models.URLField(max_length=200, blank=True)
+
     class City(models.IntegerChoices):
         tel_aviv = '1', 'Tel Aviv'
         hamerkaz = '2', 'HaMerkaz'
         hadarom = '3', 'HaDarom'
         hatzafon = '4', 'HaTzafon'
-    
+
     city = models.IntegerField(
         choices=City.choices,
         default=City.tel_aviv,
-    )   
+    )
+
 
 class UserFollowers(models.Model):
-    user_followers_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('self', on_delete=models.CASCADE)
-    follower_id	= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    follow_from = models.ForeignKey(Profile, related_name='follow_from', on_delete=models.CASCADE)
+    follow_to = models.ForeignKey(Profile, related_name='follow_to', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('follow_from', 'follow_to')
+
+    def clean(self):
+        if self.follow_from == self.follow_to:
+            raise ValueError("One Cannot follow themselves")
+
 
 class Stream(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     link = models.URLField(max_length=500, blank=False)
+    description = models.TextField(max_length=50, blank=True, null=True)
     airs_on = models.DateField()
-    ends_on = models.DateField(null=True)
+    ends_on = models.DateField(blank=True, null=True)
     added_on = models.DateField()
 
     class Meta:
-        ordering = ['airs_on', 'author']
+        ordering = ['airs_on', '-added_on', 'author']
         db_table = 'streams'
