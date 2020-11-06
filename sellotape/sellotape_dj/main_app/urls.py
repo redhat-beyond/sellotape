dfb1011e8e8d00a56115e4ff0a5b8c1802361a38
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-11-06 15:17:27 +0200

    Added 'trending' page that displays currently live streams.
    It fetches featured streams from the Twitch API and disaplys them to the user.

--- sellotape/sellotape_dj/main_app/urls.py
+++ sellotape/sellotape_dj/main_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('complete-login/', views.complete_login, name='complete-login'),
     path('', views.landing, name='landing'),
-    path('<str:username>/', views.user, name='user'),
     path('follow/<str:username>/', views.follow, name='follow'),
     path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
+    path('trending/', views.trending, name='trending'),
+    path('<str:username>/', views.user, name='user'),
 ]
