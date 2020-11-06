3c96af2b8b2213424ce7864095d3cac1231f61db
Omri Rosner <68001413+rosomri@users.noreply.github.com>
2020-11-06 17:45:53 +0200

    intagrate chat by stream

--- sellotape/sellotape_dj/main_app/urls.py
+++ sellotape/sellotape_dj/main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('complete-login/', views.complete_login, name='complete-login'),
     path('', views.landing, name='landing'),
-    path('<str:username>/', views.user, name='user'),
     path('follow/<str:username>/', views.follow, name='follow'),
     path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
+    path('<str:username>/', views.user, name='user'),
 ]
